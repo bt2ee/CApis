13d6aa9b3c454d5fdc54e7a098530515a8ea0852
chenle <chenle@forchange.tech>
2020-08-06 16:03:20 +0800

    feat: 静态属性写入 class 内部

--- src/index.js
+++ src/index.js
@@ -1,83 +1,79 @@
-import axios from 'axios'
+import axios from "axios";
 class CApis {
   constructor(serverMap, apiMap) {
-    this.serverMap = serverMap
-    this.apiMap = apiMap
+    this.serverMap = serverMap;
+    this.apiMap = apiMap;
     if (this.validate) {
       this.combine();
       this.middleware();
-      this.parse()
+      this.parse();
     } else {
-      console.error('参数不合法，请检查你的参数配置')
+      console.error("参数不合法，请检查你的参数配置");
     }
   }
+
+  static reqMiddleware = [];
+  static resMiddleware = [];
+  static useReq = function() {
+    this.reqMiddleware.push(arguments);
+  };
+  static useRes = function() {
+    this.resMiddleware.push(arguments);
+  };
 
   // 如果后期需要检查的话，可以在这里加
   get validate() {
     if (!this.serverMap || !this.apiMap) {
-      return false
+      return false;
     }
     return true;
   }
 
   // 获取 serverMap 中的 default server
   get base() {
-    let base = ''
+    let base = "";
     for (const key of Object.keys(this.serverMap)) {
       if (!base) {
-        base = key
+        base = key;
       }
       if (this.serverMap[key].default) {
-        base = key
+        base = key;
       }
-
     }
     if (!base) {
-      console.error('没有找到默认的服务器配置')
+      console.error("没有找到默认的服务器配置");
     }
-    return base
+    return base;
   }
 
   // 整合 apiMap
   combine() {
     for (const key of Object.keys(this.apiMap)) {
-      const item = this.apiMap[key]
+      const item = this.apiMap[key];
 
       if (!item.server) {
-        item.server = this.base
+        item.server = this.base;
       }
 
-      this.apiMap[key] = Object.assign({}, this.serverMap[item.server], item)
+      this.apiMap[key] = Object.assign({}, this.serverMap[item.server], item);
     }
   }
 
   // 中间件 axios 拦截器
   middleware() {
     CApis.reqMiddleware.map((middleware) => {
-      axios.interceptors.request.use(...middleware)
-    })
+      axios.interceptors.request.use(...middleware);
+    });
 
     CApis.resMiddleware.map((middleware) => {
-      axios.interceptors.response.use(...middleware)
-    })
+      axios.interceptors.response.use(...middleware);
+    });
   }
 
   // 解析 api
   parse() {
     // console.log(this.apiMap, 'apiMap')
   }
-
 }
 
-CApis.reqMiddleware = []
-CApis.resMiddleware = []
-
-CApis.useReq = function() {
-  this.reqMiddleware.push(arguments)
-}
-
-CApis.useRes = function() {
-  this.resMiddleware.push(arguments)
-}
-
-export default CApis
+export default CApis;
